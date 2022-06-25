4658388ebd08fd6f8be1709a157a70972bb677da
weston cox <coxw1@seattleu.edu>
2022-06-24 17:07:33 -0700

    Add project files.

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long ID { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+}
